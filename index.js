b47df064fef2d8b9c7fbc6f17f97e464c240b8b2
Mia Rivera <riverami@iupui.edu>
2017-06-26 16:26:12 -0400

    Handle clicks on the web page to change text

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("it worked");
+function changeHeading(ev) {
+   ev.target.textContent = 'clickity click'
+}
+const btn = document.querySelector('button')
+btn.addEventListener('click', changeHeading)
+
+const p = document.querySelector('#instructions')
+p.addEventListener('click', changeHeading)
