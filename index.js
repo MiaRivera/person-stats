eaf74a0788a853f40b4de2f6b5bf902527a90b73
Mia Rivera <riverami@iupui.edu>
2017-06-27 14:53:30 -0400

    Added color to the same form and fuser *use para graph creation

--- index.js
+++ index.js
@@ -3,20 +3,17 @@ function changeDiv(ev) {
     const f = ev.target
     const name = f.personName.value
     const age = f.personAge.value
-    const div = document.querySelector('#stats-p')
-    div.innerHTML = `${name} ${age}`
-}
+    const color = f.favoriteColor.value
+    const div = document.querySelector('#stats')
 
-function changeColor(color) {
-    color.preventDefault()
-    const f = color.target
-    const input = f.textColor.value
-    const paragraph = document.querySelector('#stats-p')
-    paragraph.style.color = input
+    const p = document.createElement('p')
+    p.textContent = `${name} ${age}`
+    p.style.color = color
+
+    div.appendChild(p)
+
+    //div.innerHTML = `${name} ${age}`
 }
 
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeDiv)
-
-const colorChange = document.querySelector('#color-choice')
-colorChange.addEventListener('submit', changeColor)
