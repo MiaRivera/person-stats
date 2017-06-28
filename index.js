d0642dea3096a6f889cd57783597aa67afa3ac5c
Mia Rivera <riverami@iupui.edu>
2017-06-28 00:13:27 -0400

    Created seperate functions to handle individual responsibilities in index.js

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 function changeDiv(ev) {
     ev.preventDefault()
     const f = ev.target
-    const name = f.personName.value
-    const age = f.personAge.value
-    const color = f.favoriteColor.value
-    const div = document.querySelector('#stats')
+    const name = f.personName
+    const age = f.personAge
+    const color = f.favoriteColor
+    
 
-    const list = document.createElement('ul')
-
-    const nameItem = document.createElement('li')
-    nameItem.textContent = `Name: ${name}`
-    list.appendChild(nameItem)
-
-    const ageItem = document.createElement('li')
-    ageItem.textContent = `Age: ${age}`
-    list.appendChild(ageItem)
-
-    const colorItem = document.createElement('li')
-    colorItem.textContent = 'Favorite Color: '
-    list.appendChild(colorItem)
-
-    const colorDiv = document.createElement('div')
-    colorDiv.style.backgroundColor = color
-    colorDiv.style.width = '6rem'
-    colorDiv.style.height = '3rem'
-    colorItem.appendChild(colorDiv)
+    renderList(name, age, color)
+    //colorItem.appendChild(renderColor)
 
     //list.textContent = `${name} ${age}`
-
-    div.appendChild(list)
-    
 
     //div.innerHTML = `${name} ${age}`
 }
 
+function renderList(name, age, color) {
+    const div = document.querySelector('#stats')
+    const list = document.createElement('ul')
+
+    list.appendChild(renderListItem(name))
+    list.appendChild(renderListItem(age))
+    list.appendChild(renderListItem(color))
+    
+    div.appendChild(list)
+}
+
 const personForm = document.querySelector('#person-form')
 personForm.addEventListener('submit', changeDiv)
+
+
+
+function renderListItem(input) {
+    const lineValue = input.value
+    const lineItem = document.createElement('li')
+
+    if(input.type === "text") {
+        console.log("found text")
+        lineItem.textContent = `Name: ${lineValue}`
+    } else if (input.type === "number") {
+        lineItem.textContent = `Age: ${lineValue}`
+    } else if (input.type === "color") {
+        lineItem.textContent = 'Favorite Color: '
+        lineItem.appendChild(renderColor(lineValue))
+    }
+
+    return lineItem
+}
+
+function renderColor(color) {
+    const colorDiv = document.createElement('div')
+
+    colorDiv.style.backgroundColor = color
+    colorDiv.style.width = '6rem'
+    colorDiv.style.height = '3rem'
+
+    return colorDiv
+}
